16523c4aa6c64f04fce7c75405eac462c01f6063
prathima murthy <prathima2005@gmail.com>
2021-06-04 21:35:37 +0530

    testrun

--- sketch.js
+++ sketch.js
@@ -20,7 +20,7 @@ function setup(){
     
     ground = new Ground(600,height,1200,20)
 
-    box1 = new Box(700,320,70,70);
+    box1 = new Box(700,320,90,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
